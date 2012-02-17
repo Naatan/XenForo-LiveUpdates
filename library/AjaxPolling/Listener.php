01f25957e9ad5e6c76b141e8b19784b4f3ccf31e
Nathan Rijksen <n.rijksen@gmail.com>
2012-02-16 22:13:34 -0500

    Made compatible with PHP versions < 5.3

--- library/AjaxPolling/Listener.php
+++ library/AjaxPolling/Listener.php
@@ -10,23 +10,23 @@ class AjaxPolling_Listener
 	 */
 	private final function __construct() {}
 
-    /**
-     * Hook into template_create to add JS dependencies
-     * 
-     * @param  string                    $templateName 
-     * @param  array                     &$params      
-     * @param  XenForo_Template_Abstract $template     
-     * 
-     * @return void
-     */
-    public static function template_create($templateName, array &$params, XenForo_Template_Abstract $template) 
-    {
-    	if ( ! static::$addJs || class_exists('XenForo_Dependencies_Admin', false))
-    	{
-    		return;
-    	}
+	/**
+	 * Hook into template_create to add JS dependencies
+	 * 
+	 * @param  string					$templateName 
+	 * @param  array					&$params	  
+	 * @param  XenForo_Template_Abstract $template	 
+	 * 
+	 * @return void
+	 */
+	public static function template_create($templateName, array &$params, XenForo_Template_Abstract $template) 
+	{
+		if ( ! self::$addJs || class_exists('XenForo_Dependencies_Admin', false))
+		{
+			return;
+		}
 
-    	$template->addRequiredExternal('js','js/ajaxpolling/poll.js');
-    }
+		$template->addRequiredExternal('js','js/ajaxpolling/poll.js');
+	}
 
 }
